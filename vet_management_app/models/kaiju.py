4f864b8a9a18a229523a75e4efff108ad7aa0339
SinnottTM <tim.sinnott86@gmail.com>
2021-03-22 10:59:08 +0000

    4th, created Vet Class, edited Pet to Kaiju

---
+++ vet_management_app/models/kaiju.py
@@ -0,0 +1,9 @@
+#  Kaiju class, 1st full version
+class Kaiju:
+    def __init__(self, name, dob, kaiju_type, owner, treatment_notes, vet_assigned):
+        self.name = name
+        self.dob = dob
+        self.kaiju_type = kaiju_type
+        self.owner = owner
+        self.treatment_notes = treatment_notes
+        self.vet_assigned = vet_assigned
---
+++ vet_management_app/models/vet.py
@@ -0,0 +1,6 @@
+#  Vet class, 1st full version
+class Vet:
+    def __init__(self, name, kaiju_assigned, kaiju_owner):
+        self.name = name
+        self.kaiju_assigned = kaiju_assigned
+        self.kaiju_owner = kaiju_owner
