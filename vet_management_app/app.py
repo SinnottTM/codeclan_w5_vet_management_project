8abdd22184d6307ea387a3b3fc53a98d18e8ecff
SinnottTM <tim.sinnott86@gmail.com>
2021-03-22 13:27:29 +0000

    13th, app (c)

--- vet_management_app/app.py
+++ vet_management_app/app.py
@@ -1,15 +1,15 @@
 # app file, TBC
 
-# from flask import Flask, render_template
-# from controllers.pet_controller import pet_blueprint
+from flask import Flask, render_template
+from controllers.kaiju_controller import kaiju_blueprint
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
-# app.register_blueprint(pet_blueprint)
+app.register_blueprint(kaiju_blueprint)
 
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
