a246cc7ac09f73338ff3b1be39c30af1d395a56d
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 22:19:04 +0000

    47th, misc updates

--- vet_management_app/app.py
+++ vet_management_app/app.py
@@ -2,10 +2,12 @@
 
 from flask import Flask, render_template
 from controllers.kaiju_controller import kaiju_blueprint
+from controllers.vet_controller import vet_blueprint
 
 app = Flask(__name__)
 
 app.register_blueprint(kaiju_blueprint)
+# app.register_blueprint(vet_blueprint)
 
 @app.route('/')
 def home():
