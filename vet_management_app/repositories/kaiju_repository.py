c3d7b1f804c17a35b4526d85d46392b7af870d59
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 11:28:44 +0000

    33th, kaiju repo updated

--- vet_management_app/repositories/kaiju_repository.py
+++ vet_management_app/repositories/kaiju_repository.py
@@ -1,19 +1,18 @@
-# Kaiju repo, 1st attempt.
+# Vet repo, got assist by Malcolm, will assume all current code is working fine!
 from db.run_sql import run_sql
 
 from models.kaiju import Kaiju
-import repositories.kaiju_repository as kaiju_repository
 
 # CRUD
 
 #CREATE
 def save(kaiju):
-    sql = "INSERT INTO kaiju( name, dob, type, vet_id, treatment_notes ) VALUES ( %s, %s, %s, %s, %s, %s ) RETURNING *"
-    values = [kaiju.name, kaiju.dob, kaiju.kaiju_type, kaiju.vet_assigned, kaiju.treatment_notes]
+    sql = "INSERT INTO kaiju( name, dob, kaiju_type, treatment_notes ) VALUES ( %s, %s, %s, %s ) RETURNING *"
+    values = [kaiju.name, kaiju.dob, kaiju.kaiju_type, kaiju.treatment_notes]
     results = run_sql(sql, values)
     id = results[0]['id']
     kaiju.id = id
-    return kaiju
+    # return kaiju
 
 #READ 
 def select_all_kaiju():
@@ -23,8 +22,8 @@ def select_all_kaiju():
     results = run_sql(sql)
 
     for row in results:
-        kaiju = Kaiju(row['name'], row['dob'], row['type'], row['vet_id'],
-                        row['treatment_notes'])
+        kaiju = Kaiju(row['name'], row['dob'], row['kaiju_type'],
+                        row['treatment_notes'], row['id'])
         kaiju_list.append(kaiju)
     return kaiju_list
 
@@ -35,14 +34,14 @@ def select_single_kaiju(id):
     result = run_sql(sql, values)[0]
 
     if result is not None:
-        kaiju = Kaiju(kaiju.name, kaiju.dob, kaiju.kaiju_type, kaiju.vet_assigned, kaiju.treatment_notes)
+        kaiju = Kaiju(result['kaiju.name'], result['kaiju.dob'], result['kaiju.kaiju_type'], result['kaiju.treatment_notes'], result['kaiju.id'])
     return kaiju
 
 #UPDATE
 def update(kaiju):
-    sql = "UPDATE kaiju SET (name, dob, type, owner_id, vet_id, treatment_notes) = (%s, %s, %s, %s, %s, %s) WHERE id = %s"
+    sql = "UPDATE kaiju SET (name, dob, kaiju_type, treatment_notes) = (%s, %s, %s, %s ) WHERE id = %s"
     values = [kaiju.name, kaiju.dob, kaiju.kaiju_type,
-              kaiju.kaiju_owner, kaiju.vet_assigned, kaiju.treatment_notes]
+              kaiju.treatment_notes, kaiju.id]
     run_sql(sql, values)
 
 #DELETE
