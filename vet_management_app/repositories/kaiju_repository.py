9ee132aad46e73d4958df0c2875eff680e337b36
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 10:35:08 +0000

    29th, kaiju repo updated

--- vet_management_app/repositories/kaiju_repository.py
+++ vet_management_app/repositories/kaiju_repository.py
@@ -8,9 +8,8 @@ import repositories.kaiju_repository as kaiju_repository
 
 #CREATE
 def save(kaiju):
-    sql = "INSERT INTO kaiju( name, dob, type, owner_id, vet_id, treatment_notes ) VALUES ( %s, %s, %s, %s, %s, %s ) RETURNING *"
-    values = [kaiju.name, kaiju.dob, kaiju.kaiju_type,
-              kaiju.kaiju_owner, kaiju.vet_assigned, kaiju.treatment_notes]
+    sql = "INSERT INTO kaiju( name, dob, type, vet_id, treatment_notes ) VALUES ( %s, %s, %s, %s, %s, %s ) RETURNING *"
+    values = [kaiju.name, kaiju.dob, kaiju.kaiju_type, kaiju.vet_assigned, kaiju.treatment_notes]
     results = run_sql(sql, values)
     id = results[0]['id']
     kaiju.id = id
@@ -24,7 +23,7 @@ def select_all_kaiju():
     results = run_sql(sql)
 
     for row in results:
-        kaiju = Kaiju(row['name'], row['dob'], row['type'], row['owner_id'], row['vet_id'],
+        kaiju = Kaiju(row['name'], row['dob'], row['type'], row['vet_id'],
                         row['treatment_notes'])
         kaiju_list.append(kaiju)
     return kaiju_list
@@ -36,8 +35,7 @@ def select_single_kaiju(id):
     result = run_sql(sql, values)[0]
 
     if result is not None:
-        kaiju = Kaiju(kaiju.name, kaiju.dob, kaiju.kaiju_type,
-                      kaiju.kaiju_owner, kaiju.vet_assigned, kaiju.treatment_notes)
+        kaiju = Kaiju(kaiju.name, kaiju.dob, kaiju.kaiju_type, kaiju.vet_assigned, kaiju.treatment_notes)
     return kaiju
 
 #UPDATE
