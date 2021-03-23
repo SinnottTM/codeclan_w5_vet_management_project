87dafefde310a00dbb3bb55b6cde0ce0de44f7c4
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 10:32:13 +0000

    24th, vet class updated

--- vet_management_app/models/vet.py
+++ vet_management_app/models/vet.py
@@ -1,5 +1,6 @@
 #  Vet class, 1st full version
 class Vet:
-    def __init__(self, name, kaiju_assigned):
+    def __init__(self, name, kaiju_id):
         self.name = name
-        self.kaiju_assigned = None
+        self.kaiju_id = None
+        
