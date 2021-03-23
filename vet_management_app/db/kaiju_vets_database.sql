3ddbc2b072fda90296415ce9594c59585b4bf192
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 10:31:35 +0000

    23th, sql db updated

--- vet_management_app/db/kaiju_vets_database.sql
+++ vet_management_app/db/kaiju_vets_database.sql
@@ -1,30 +1,15 @@
--- Database SQL page
--- Note: May need to reorder drops for referencing etc?
 DROP TABLE IF EXISTS kaiju;
 DROP TABLE IF EXISTS vets;
-DROP TABLE If EXISTS owner;
 
 CREATE TABLE vets ( 
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    kaiju_id INT REFERENCES kaiju(id) ON DELETE CASCADE, 
-    owner VARCHAR(255)
-
-);
-
-CREATE TABLE owners (
-    id SERIAL PRIMARY KEY,
     name VARCHAR(255)
-
 );
-
 CREATE TABLE kaiju ( 
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     dob VARCHAR(255),
     type VARCHAR(255),
-    owner VARCHAR(255),
-    vet_id INT REFERENCES vet(id) ON DELETE CASCADE,
+    vet_id INT REFERENCES vets(id) ON DELETE CASCADE,
     treatment_notes TEXT
-
 );
