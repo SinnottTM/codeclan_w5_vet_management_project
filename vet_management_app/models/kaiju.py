71c22a0aeb5b3274fc66db47e61f29d3beff1450
SinnottTM <tim.sinnott86@gmail.com>
2021-03-23 10:31:56 +0000

    23th, kaiju class updated

--- vet_management_app/models/kaiju.py
+++ vet_management_app/models/kaiju.py
@@ -1,15 +1,12 @@
-#  Kaiju class, 1st full version
+#  Kaiju class, 2nd full version
 class Kaiju:
-    def __init__(self, name, dob, kaiju_type, kaiju_owner, kaiju_vet, treatment_notes):
+    def __init__(self, name, dob, kaiju_type, kaiju_vet, treatment_notes):
         self.name = name
         self.dob = dob
         self.kaiju_type = kaiju_type
-        self.kaiju_owner = None
         self.kaiju_vet = None
         self.treatment_notes = treatment_notes
 
     def assign_vet(self, kaiju_vet):
         self.kaiju_vet = kaiju_vet
 
-    def assign_owner(self, kaiju_owner):
-        self.kaiju_owner = kaiju_owner
