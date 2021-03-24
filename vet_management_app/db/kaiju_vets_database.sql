445c39170b4641ea0cca4318aab931c7fc2f7f81
SinnottTM <tim.sinnott86@gmail.com>
2021-03-24 11:51:09 +0000

    56th, SQL Edit

--- vet_management_app/db/kaiju_vets_database.sql
+++ vet_management_app/db/kaiju_vets_database.sql
@@ -1,28 +1,17 @@
-DROP TABLE IF EXISTS vets;
 DROP TABLE IF EXISTS kaiju;
+DROP TABLE IF EXISTS vets;
+
+
+CREATE TABLE vets ( 
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
 
 CREATE TABLE kaiju ( 
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     dob VARCHAR(255),
     kaiju_type VARCHAR(255),
-    treatment_notes TEXT
+    treatment_notes TEXT,
+    vet_id INT REFERENCES vets(id) ON DELETE CASCADE
 );
-
-CREATE TABLE vets ( 
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    kaiju_id INT REFERENCES kaiju(id) ON DELETE CASCADE
-);
-
--- CREATE TABLE bookings (
---     id SERIAL PRIMARY KEY,
---     monday BOOLEAN,
---     tuesday BOOLEAN,
---     wednesday BOOLEAN,
---     thursday BOOLEAN,
---     friday BOOLEAN,
---     morning BOOLEAN,
---     afternoon BOOLEAN,
---     vet_id INT REFERENCES vets(id) ON DELETE CASCADE
--- );
