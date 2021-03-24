41322b75ac0dd8f034214f188b63ad9d163306af
SinnottTM <tim.sinnott86@gmail.com>
2021-03-24 22:22:47 +0000

    69th - app file updated

--- vet_management_app/app.py
+++ vet_management_app/app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 app.register_blueprint(kaiju_blueprint)
 app.register_blueprint(vet_blueprint)
 
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    kaiju_to_update = kaiju_vets_database.query.get_or_404(id)
+
 @app.route('/')
 def home():
     return render_template('index.html')
